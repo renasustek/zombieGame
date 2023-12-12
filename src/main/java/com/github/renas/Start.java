3db89ed27f0c7ee2ed380793a83ef1270d65be1e
renasustek <renasustek@gmail.com>
2023-12-12 23:59:26 +0000

    creating a very simple text based game to see how i could improve it the user respones could replaced with enums as they will be reused.

--- src/main/java/com/github/renas/Start.java
+++ src/main/java/com/github/renas/Start.java
@@ -1,5 +1,35 @@
 package com.github.renas;
 
-public class Start {
+import java.util.Scanner;
 
+public class Start {
+    public Start() {
+        System.out.println("Welcome to ZombieLand, rules: DONT DIE.  Options will be in upper case");
+        System.out.println("You wake up in a small house, you look outside and there is one zombie walking around" +
+                "on the table there is a gun and 7 bullets");
+        System.out.println("Do you TAKE the gun and the bullets?");
+//        System.out.println("Do you EXIT the small house");
+
+        String input1;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            input1 = scanner.nextLine();
+        } while (input1 != "TAKE");
+
+        if (input1 =="TAKE"){
+            System.out.println("add gun and bullets to inventory");
+        }
+
+        System.out.println("you exit the house, do you ATTACK the zombie or SNEAK away from it?");
+        Scanner scanner = new Scanner(System.in);
+        String input2 = scanner.nextLine();
+
+        switch (input2){
+            case "ATTACK":
+                System.out.println("attack zombie class here");
+                break;
+            case "SNEAK":
+                System.out.println("sneak class here");
+        }
+    }
 }
